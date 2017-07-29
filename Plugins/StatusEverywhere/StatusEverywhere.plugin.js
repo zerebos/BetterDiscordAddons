bb4285fe0defd55235de5f42f7aa1d13ca1ad9d3
Zack Rauen <rauenzi@outlook.com>
2017-07-28 22:23:05 -0400

    Should be less laggy

--- Plugins/StatusEverywhere/StatusEverywhere.plugin.js
+++ Plugins/StatusEverywhere/StatusEverywhere.plugin.js
@@ -2,88 +2,7 @@
 
 var StatusEverywhere = (function() {
 
-class SettingField {
-	constructor(name, helptext) {
-		this.name = name;
-		this.helptext = helptext;
-		this.row = $("<div>");
-		this.row.attr("class", "ui-flex flex-vertical flex-justify-start flex-align-stretch flex-nowrap ui-switch-item");
-		this.row.css("margin-top", 0);
-		this.top = $("<div>");
-		this.top.attr("class", "ui-flex flex-horizontal flex-justify-start flex-align-stretch flex-nowrap")
-		this.label = $("<h3>");
-		this.label.attr("class", "ui-form-title h3 margin-reset margin-reset ui-flex-child");
-		this.label.text(name);
-		
-		this.help = $("<div>");
-		this.help.attr("class", "ui-form-text style-description margin-top-4");
-		this.help.css("flex", "1 1 auto");
-		this.help.text(helptext);
-		
-		this.top.append(this.label);
-		this.row.append(this.top);
-		this.row.append(this.help);
-	}
-}
-
-class TextSetting extends SettingField {
-	constructor(label, help, value, placeholder, callback) {
-		super(label, help);
-		var input = $("<input>", {
-			type: "text",
-			placeholder: placeholder,
-			value: value
-		});
-
-		input.on("keyup."+appNameShort+" change."+appNameShort, function() {
-			if (typeof callback != 'undefined') {
-				callback($(this).val())
-			}
-		})
-		
-		this.top.append(input);
-		return this.row;
-	}
-}
-
-class CheckboxSetting extends SettingField {
-	constructor(label, help, isChecked, callback, disabled) {
-		super(label, help);
-		var isDisabled = false
-		if (typeof disabled != 'undefined') isDisabled = disabled;
-		var input = $("<input>", {
-			type: "checkbox",
-			checked: isChecked,
-			disabled: isDisabled
-		});
-		input.attr("class", "ui-switch-checkbox");
-
-		input.on("click."+appNameShort, function() {
-			var checked = $(this).prop("checked");
-			if (checked) {
-				switchDiv.addClass("checked");
-			}
-			else {
-				switchDiv.removeClass("checked");
-			}
-			
-			if (typeof callback != 'undefined') {
-				callback(checked)
-			}
-		})
-		
-		var checkboxWrap = $('<label class="ui-switch-wrapper ui-flex-child" style="flex:0 0 auto;">');
-		checkboxWrap.append(input);
-		var switchDiv = $('<div class="ui-switch">');
-		if (isChecked) switchDiv.addClass("checked");
-		checkboxWrap.append(switchDiv);
-		
-		this.top.append(checkboxWrap);
-		return this.row;
-	}
-}
-
-class StatusEverywhere {
+class Plugin {
 	getName(){return "Status Everywhere"}
 	getShortName() {return "StatusEverywhere"}
 	getDescription(){return "Adds user status everywhere Discord doesn't."}
@@ -144,15 +63,15 @@ class StatusEverywhere {
 		if (elem.parents(".messages.scroller").length || elem.find(".message-group").parents(".messages.scroller").length) {
 			setTimeout(() => {
 				elem.find('.avatar-large').each((index, elem) => {
-					if (!$(elem).find('.status').length) $(elem).append(this.getAuthorStatus(elem))
+					setTimeout( () => {	if (!$(elem).find('.status').length) $(elem).append(this.getAuthorStatus(elem)) }, 1)
 				})
-			},20)
+			},1)
 		}
 
 	}
 }
 
 
-return StatusEverywhere
+return Plugin
 })();
 
