01616298580d510346007800b5d9d1034a072ca5
Zack Rauen <rauenzi@outlook.com>
2017-07-31 12:00:55 -0400

    Smoothen transitions

--- Plugins/BlurNSFW/BlurNSFW.plugin.js
+++ Plugins/BlurNSFW/BlurNSFW.plugin.js
@@ -13,13 +13,14 @@ class Plugin {
 	unload(){}
 	
 	start(){
-		this.style = `:root {--blur-nsfw: 10px;}
+		this.style = `:root {--blur-nsfw: 10px; --blur-nsfw-time: 200ms;}
 		.attachment-image img.blur:hover, .embed-thumbnail img.blur:hover, .attachment-image canvas.blur:hover, .embed-thumbnail canvas.blur:hover {
-			transition: 200ms cubic-bezier(.2, .11, 0, 1) !important;
+			transition: var(--blur-nsfw-time) cubic-bezier(.2, .11, 0, 1) !important;
 			filter: blur(0px);
 		}
 		.attachment-image img.blur, .embed-thumbnail img.blur, .attachment-image canvas.blur, .embed-thumbnail canvas.blur {
-			filter: blur(var(--blur-nsfw))
+			filter: blur(var(--blur-nsfw));
+			transition: var(--blur-nsfw-time) cubic-bezier(.2, .11, 0, 1) !important;
 		}`
 		BdApi.injectCSS(this.getShortName(), this.style)
 		this.blurStuff()
@@ -37,7 +38,7 @@ class Plugin {
 		return this.getReactInstance($('.title-wrap')[0])._currentElement.props.children[2].props.channel.nsfw
 	}
 
-	blurStuff(node) {
+	blurStuff() {
 		if (!this.isNSFWChannel()) return;
 
 		var blurAction = (index, elem) => {
@@ -51,7 +52,7 @@ class Plugin {
 		$('.embed-thumbnail canvas').each(blurAction)
 	}
 
-	unblurStuff(node) {
+	unblurStuff() {
 		var unblurAction = (index, elem) => {
 			var img = $(elem)
 			if (img.hasClass("blur")) img.removeClass("blur");
@@ -82,7 +83,7 @@ class Plugin {
 		var panel = $("<form>").addClass("form").css("width", "100%");
 		var header = $('<div class="formNotice-2tZsrh margin-bottom-20 padded card-3DrRmC">')//.css("background", SettingField.getAccentColor()).css("border-color", "transparent")
 		var headerText = $('<div class="default-3bB32Y formText-1L-zZB formNoticeBody-1C0wup whiteText-32USMe modeDefault-389VjU primary-2giqSn">')
-		headerText.html("To update the blur amount change the css variable <span style='font-family: monospace;'>--blur-nsfw</span> to something like <span style='font-family: monospace;'>20px</span>")
+		headerText.html("To update the blur amount change the css variable <span style='font-family: monospace;'>--blur-nsfw</span> to something like <span style='font-family: monospace;'>20px</span>. <br> You can also change the tranistion time by changing <span style='font-family: monospace;'>--blur-nsfw-time</span> to something like <span style='font-family: monospace;'>500ms</span>")
 		headerText.css("line-height", "150%")
 		headerText.appendTo(header)
 		header.appendTo(panel)
