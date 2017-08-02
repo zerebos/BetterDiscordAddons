721cc7f390a9e092a09bcdf379e128a16972b402
Zack Rauen <rauenzi@outlook.com>
2017-08-02 17:47:05 -0400

    Fix embed issues

--- Plugins/BetterRoleColors/BetterRoleColors.plugin.js
+++ Plugins/BetterRoleColors/BetterRoleColors.plugin.js
@@ -409,7 +409,7 @@ class Plugin {
 	    $(".message-group .message").each((index, elem) => {
 	    	var messageNum = $(elem).index()
 	    	var instance = this.getReactInstance(elem)
-	    	$(elem).find('.mention:contains("@")').each((index, elem) => {
+	    	$(elem).find('.message-text > .markup > .mention:contains("@")').each((index, elem) => {
 	        	var users = instance._hostParent._currentElement.props.children[0][messageNum].props.message.content.match(/<@!?[0-9]+>/g)
 	        	var user = users[index].replace(/<|@|!|>/g, "")
 	        	var textColor = this.getColorData(server, user)
