5814182ad9342711c24f640f87c7f32d990439c1
Zack Rauen <rauenzi@outlook.com>
2018-11-09 23:51:36 -0500

    rewrite DNT

--- Plugins/DoNotTrack/CHANGELOG.md
+++ Plugins/DoNotTrack/CHANGELOG.md
@@ -1,5 +1,9 @@
 # DoNotTrack Changelog
 
+### 0.0.2
+
+ - Rewrite to new lib
+
 ### 0.0.1
 
  - Initial version
--- Plugins/DoNotTrack/DoNotTrack.plugin.js
+++ Plugins/DoNotTrack/DoNotTrack.plugin.js
@@ -1,55 +1,78 @@
-//META{"name":"DoNotTrack","website":"https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/DoNotTrack","source":"https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/DoNotTrack/DoNotTrack.plugin.js"}*//
+//META{"name":"DoNotTrack","displayName":"DoNotTrack","website":"https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/DoNotTrack","source":"https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/DoNotTrack/DoNotTrack.plugin.js"}*//
 
-class DoNotTrack {
-	getName() { return "DoNotTrack"; }
-	getShortName() { return "DoNotTrack"; }
-	getDescription() { return "Stops Discord from tracking everything you do like Sentry and Analytics. Support Server: bit.ly/ZeresServer"; }
-	getVersion() { return "0.0.1"; }
-	getAuthor() { return "Zerebos"; }
+var DoNotTrack = (() => {
+	if (!global.ZLibrary && !global.ZLibraryPromise) global.ZLibraryPromise = new Promise((resolve, reject) => {
+		require("request").get({url: "https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js", timeout: 10000}, (err, res, body) => {
+			if (err || 200 !== res.statusCode) return reject(err || res.statusMessage);
+			try {const vm = require("vm"), script = new vm.Script(body, {displayErrors: true}); resolve(script.runInThisContext());}
+			catch(err) {reject(err);}
+		});
+	});
+	const config = {"info":{"name":"DoNotTrack","authors":[{"name":"Zerebos","discord_id":"249746236008169473","github_username":"rauenzi","twitter_username":"ZackRauen"}],"version":"0.0.2","description":"Stops Discord from tracking everything you do like Sentry and Analytics. Support Server: bit.ly/ZeresServer","github":"https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/DoNotTrack","github_raw":"https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/DoNotTrack/DoNotTrack.plugin.js"},"changelog":[{"title":"What's New?","items":["Rewrite to new library."]}],"main":"index.js"};
+	const compilePlugin = ([Plugin, Api]) => {
+		const plugin = (Plugin, Api) => {
+    const {Patcher, WebpackModules} = Api;
+
+    return class DoNotTrack extends Plugin {
+        onStart() {
+            (function (Api) {
+    if (window.ZeresPluginLibrary) return; // they already have it
+    const hasShownAnnouncement = Api.PluginUtilities.loadData(this.getName(), "announcements", {localLibNotice: false}).localLibNotice;
+    if (hasShownAnnouncement) return;
+    Api.Modals.showConfirmationModal("Local Library Notice", Api.DiscordModules.React.createElement("span", null, `This version of ${this.getName()} is the final version that will be released using a remotely loaded library. Future versions will require my local library that gets placed in the plugins folder.`, Api.DiscordModules.React.createElement("br"), Api.DiscordModules.React.createElement("br"), "You can download the library now to be prepared, or wait until the next version which will prompt you to download it."), {
+        confirmText: "Download Now",
+        cancelText: "Wait",
+        onConfirm: () => {
+            require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+        }
+    });
+    Api.PluginUtilities.saveData(this.getName(), "announcements", {localLibNotice: true});
+})(Api);
+            const Analytics = WebpackModules.getByProps("AnalyticEventConfigs");
+            Patcher.instead(Analytics.default, "track", () => {});
+    
+            const Warning = WebpackModules.getByProps("consoleWarning");
+            Patcher.instead(Warning, "consoleWarning", () => {});
+    
+            const MethodWrapper = WebpackModules.getByProps("wrapMethod");
+            Patcher.instead(MethodWrapper, "wrapMethod", () => {});
+    
+            const Sentry = WebpackModules.getByProps("_originalConsoleMethods", "_wrappedBuiltIns");
+            Sentry.uninstall();
+            Patcher.instead(Sentry, "_breadcrumbEventHandler", () => () => {});
+            Patcher.instead(Sentry, "captureBreadcrumb", () => {});
+            Patcher.instead(Sentry, "_makeRequest", () => {});
+            Patcher.instead(Sentry, "_sendProcessedPayload", () => {});
+            Patcher.instead(Sentry, "_send", () => {});
+            Object.assign(window.console, Sentry._originalConsoleMethods);
+        }
+        
+        onStop() {
+            Patcher.unpatchAll();
+        }
+
+    };
+};
+		return plugin(Plugin, Api);
+	};
 	
-	load() {}
-	
-	async start() {
-        let libraryScript = document.getElementById('zeresLibraryScript');
-		if (!window.ZeresLibraryPromise && libraryScript) window.ZeresLibraryPromise = new Promise(resolve => libraryScript.addEventListener("load", resolve));
-		if (!libraryScript || (window.ZeresLibrary && window.ZeresLibrary.isOutdated)) {
-			if (libraryScript) libraryScript.parentElement.removeChild(libraryScript);
-			libraryScript = document.createElement("script");
-			libraryScript.setAttribute("type", "text/javascript");
-			libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-			libraryScript.setAttribute("id", "zeresLibraryScript");
-            document.head.appendChild(libraryScript);
-			window.ZeresLibraryPromise = new Promise(resolve => libraryScript.addEventListener("load", resolve));
+	return !global.ZLibrary ? class {
+		getName() {return config.info.name.replace(" ", "");} getAuthor() {return config.info.authors.map(a => a.name).join(", ");} getDescription() {return config.info.description;} getVersion() {return config.info.version;} stop() {}
+		showAlert() {window.BdApi.alert("Loading Error",`Something went wrong trying to load the library for the plugin. You can try using a local copy of the library to fix this.<br /><br /><a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);}
+		async load() {
+			try {await global.ZLibraryPromise;}
+			catch(err) {return this.showAlert();}
+			const vm = require("vm"), plugin = compilePlugin(global.ZLibrary.buildPlugin(config));
+			try {new vm.Script(plugin, {displayErrors: true});} catch(err) {return bdpluginErrors.push({name: this.getName(), file: this.getName() + ".plugin.js", reason: "Plugin could not be compiled.", error: {message: err.message, stack: err.stack}});}
+			global[this.getName()] = plugin;
+			try {new vm.Script(`new global["${this.getName()}"]();`, {displayErrors: true});} catch(err) {return bdpluginErrors.push({name: this.getName(), file: this.getName() + ".plugin.js", reason: "Plugin could not be constructed", error: {message: err.message, stack: err.stack}});}
+			bdplugins[this.getName()].plugin = new global[this.getName()]();
+			bdplugins[this.getName()].plugin.load();
 		}
-
-		await window.ZeresLibraryPromise;
-		this.initialize();
-	}
-
-	stop() {
-		Patcher.unpatchAll(this.getName());
-	}
-
-	initialize() {
-        PluginUtilities.checkForUpdate(this.getName(), this.getVersion());
-
-        const Analytics = InternalUtilities.WebpackModules.findByUniqueProperties(["AnalyticEventConfigs"]);
-        Patcher.instead(this.getName(), Analytics.default, "track", () => {});
-
-        const Warning = InternalUtilities.WebpackModules.findByUniqueProperties(["consoleWarning"]);
-        Patcher.instead(this.getName(), Warning, "consoleWarning", () => {});
-
-        const MethodWrapper = InternalUtilities.WebpackModules.findByUniqueProperties(["wrapMethod"]);
-        Patcher.instead(this.getName(), MethodWrapper, "wrapMethod", () => {});
-
-        const Sentry = InternalUtilities.WebpackModules.findByUniqueProperties(["_originalConsoleMethods", "_wrappedBuiltIns"]);
-        Sentry.uninstall();
-        Patcher.instead(this.getName(), Sentry, "_breadcrumbEventHandler", () => () => {});
-        Patcher.instead(this.getName(), Sentry, "captureBreadcrumb", () => {});
-        Patcher.instead(this.getName(), Sentry, "_makeRequest", () => {});
-        Patcher.instead(this.getName(), Sentry, "_sendProcessedPayload", () => {});
-        Patcher.instead(this.getName(), Sentry, "_send", () => {});
-        Object.assign(window.console, Sentry._originalConsoleMethods);
-	}
-}
-
+		async start() {
+			try {await global.ZLibraryPromise;}
+			catch(err) {return this.showAlert();}
+			bdplugins[this.getName()].plugin.start();
+		}
+	} : compilePlugin(global.ZLibrary.buildPlugin(config));
+})();
