9cf55b33f447233f9fb7275c5e5f31ac898d6e22
Zack Rauen <rauenzi@outlook.com>
2019-05-27 23:34:16 -0400

    Updated StatusEverywhere

--- src/StatusEverywhere/index.js
+++ src/StatusEverywhere/index.js
@@ -1,36 +1,37 @@
 
 module.exports = (Plugin, Api) => {
-    const {Patcher, WebpackModules, DiscordModules, Modals, PluginUtilities} = Api;
+    const {Patcher, WebpackModules, DiscordModules, PluginUtilities} = Api;
+
+    const Flux = WebpackModules.getByProps("connectStores");
+    const StatusStore = DiscordModules.UserStatusStore;
+
     return class StatusEverywhere extends Plugin {
+
         onStart() {
-			this.showAnnouncement();
+			PluginUtilities.addStyle(this.getName(), `.channels-Ie2l6A .avatar-3bWpYy { position: relative; }`);
             const Avatar = WebpackModules.getByProps("AvatarWrapper");
             const original = Avatar.default;
-            Patcher.before(Avatar, "default", (_, args) => {
+            Patcher.after(Avatar, "default", (_, args, returnValue) => {
                 if (args[0].status) return;
-                const id = args[0].src.split("/")[4];
-                const cached = DiscordModules.UserStatusStore.getStatus(id);
-				args[0].status = cached == "offline" ? DiscordModules.UserStatusStore.getStatus(id, DiscordModules.SelectedGuildStore.getGuildId()) : cached;
+                const props = args[0];
+                const id = props.src.split("/")[4];
+                const fluxWrapper = Flux.connectStores([StatusStore], () => ({status: StatusStore.getStatus(id)}));
+                const wrappedStatus = fluxWrapper(({status}) => {
+                    return DiscordModules.React.createElement(Avatar.AvatarStatusIcon, {
+                        isTyping: false,
+                        shouldAnimateTypingIndicator: false,
+                        size: props.size,
+                        status: status
+                    });
+                });
+                returnValue.props.children[1] = DiscordModules.React.createElement(wrappedStatus);
             });
             Object.assign(Avatar.default, original);
         }
-        
+
         onStop() {
+            PluginUtilities.removeStyle(this.getName());
             Patcher.unpatchAll();
-        }
-		
-		showAnnouncement() {
-            if (window.ZeresPluginLibrary) return; // they already have it
-            const hasShownAnnouncement = PluginUtilities.loadData(this.getName(), "announcements", {localLibNotice: false}).localLibNotice;
-            if (hasShownAnnouncement) return;
-            Modals.showConfirmationModal("Local Library Notice", DiscordModules.React.createElement("span", null, `This version of ${this.getName()} is the final version that will be released using a remotely loaded library. Future versions will require my local library that gets placed in the plugins folder.`, DiscordModules.React.createElement("br"), DiscordModules.React.createElement("br"), "You can download the library now to be prepared, or wait until the next version which will prompt you to download it."), {
-                confirmText: "Download Now",
-                cancelText: "Wait",
-                onConfirm: () => {
-                    require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                }
-            });
-            PluginUtilities.saveData(this.getName(), "announcements", {localLibNotice: true});
         }
 
     };
