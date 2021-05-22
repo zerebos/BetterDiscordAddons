db686bcce2abe806bb0b5b6d0ac2dba5c43dc473
cyleja1234 <43104779+cyleja1234@users.noreply.github.com>
2021-05-21 22:36:06 -0600

    Update AccountDetailsPlus.plugin.js
    added settings to flip mouse interactions

--- Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js
+++ Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js
@@ -10,31 +10,127 @@
  */
 /*@cc_on
 @if (@_jscript)
-    
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-    }
-    WScript.Quit();
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
 
 @else@*/
 
 module.exports = (() => {
-    const config = {info:{name:"AccountDetailsPlus",authors:[{name:"Zerebos",discord_id:"249746236008169473",github_username:"rauenzi",twitter_username:"ZackRauen"}],version:"0.1.10",description:"Lets you view popout, nickname and more from your account panel at the bottom.",github:"https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/AccountDetailsPlus",github_raw:"https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js"},changelog:[{title:"Bugs Squashed",type:"fixed",items:["Fix conflicting popouts.","Stopped using deprecated functions."]}],main:"index.js",defaultConfig:[{type:"category",id:"popout",name:"User Popout",collapsible:true,shown:false,settings:[{type:"switch",id:"avatar",name:"Avatar",note:"Opens your popout when clicking your avatar.",value:true},{type:"switch",id:"username",name:"Username",note:"Opens your popout when clicking your username.",value:true}]},{type:"category",id:"statusPicker",name:"Status Picker",collapsible:true,shown:false,settings:[{type:"switch",id:"avatar",name:"Avatar",note:"Opens your popout when right clicking your avatar.",value:true},{type:"switch",id:"username",name:"Username",note:"Opens your popout when right clicking your username.",value:true}]},{type:"category",id:"nickname",name:"Nickname",collapsible:true,shown:false,settings:[{type:"dropdown",id:"showNickname",name:"Name Shown",value:true,options:[{label:"Username",value:false},{label:"Nickname",value:true}]},{type:"switch",id:"oppositeOnHover",name:"Opposite On Hover",note:"Shows the opposite on hover. e.g. if you are showing nickname, hovering will show your username.",value:true}]}]};
-
+    const config = {
+        info: {
+            name: "AccountDetailsPlus",
+            authors: [{
+                name: "Zerebos",
+                discord_id: "249746236008169473",
+                github_username: "rauenzi",
+                twitter_username: "ZackRauen"
+            }],
+            version: "0.1.10",
+            description: "Lets you view popout, nickname and more from your account panel at the bottom.",
+            github: "https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/AccountDetailsPlus",
+            github_raw: "https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js"
+        },
+        changelog: [{
+            title: "Bugs Squashed",
+            type: "fixed",
+            items: ["Fix conflicting popouts.", "Stopped using deprecated functions."]
+        }],
+        main: "index.js",
+        defaultConfig: [{
+            type: "category",
+            id: "popout",
+            name: "User Popout",
+            collapsible: true,
+            shown: false,
+            settings: [{
+                type: "switch",
+                id: "avatar",
+                name: "Avatar",
+                note: "Opens your popout when clicking your avatar.",
+                value: true
+            }, {
+                type: "switch",
+                id: "username",
+                name: "Username",
+                note: "Opens your popout when clicking your username.",
+                value: true
+            }]
+        }, {
+            type: "category",
+            id: "statusPicker",
+            name: "Status Picker",
+            collapsible: true,
+            shown: false,
+            settings: [{
+                type: "switch",
+                id: "avatar",
+                name: "Avatar",
+                note: "Opens your popout when right clicking your avatar.",
+                value: true
+            }, {
+                type: "switch",
+                id: "username",
+                name: "Username",
+                note: "Opens your popout when right clicking your username.",
+                value: true
+            }]
+        }, {
+            type: "category",
+            id: "nickname",
+            name: "Nickname",
+            collapsible: true,
+            shown: false,
+            settings: [{
+                type: "dropdown",
+                id: "showNickname",
+                name: "Name Shown",
+                value: true,
+                options: [{
+                    label: "Username",
+                    value: false
+                }, {
+                    label: "Nickname",
+                    value: true
+                }]
+            }, {
+                type: "switch",
+                id: "oppositeOnHover",
+                name: "Opposite On Hover",
+                note: "Shows the opposite on hover. e.g. if you are showing nickname, hovering will show your username.",
+                value: true
+            }]
+        }, {
+            type: "category",
+            id: "flip",
+            name: "Flip Mouse",
+            collapsible: false,
+            shown: false,
+            settings: [{
+                type: "switch",
+                id: "FlipMouseInteractions",
+                name: "Flip Mouse Interactions",
+                note: "switches left and right mosue buttons. (this DOESN'T change the name description of other settings!)",
+                value: true
+            }]
+        }]
+    };
     return !global.ZeresPluginLibrary ? class {
         constructor() {this._config = config;}
         getName() {return config.info.name;}
@@ -117,15 +213,15 @@ module.exports = (() => {
             if (!this.tagElement) return Logger.err("No tag element to remove listeners");
             if (!this.avatarElement) return Logger.err("No avatar element to remove listeners");
             this.tagElement.removeEventListener("mousedown", this.updateIsPopoutOpen);
-            this.tagElement.removeEventListener("click", this.showUserPopout);
-            this.tagElement.removeEventListener("contextmenu", this.showStatusPicker);
+            this.tagElement.removeEventListener(this.settings.flip?"contextmenu":"click", this.showUserPopout);//originaly click
+            this.tagElement.removeEventListener(this.settings.flip?"click":"contextmenu", this.showStatusPicker);//originaly contextmenu
 
             this.avatarElement.removeEventListener("mousedown", this.updateIsPopoutOpen);
-            this.avatarElement.removeEventListener("click", this.showUserPopout);
-            this.avatarElement.removeEventListener("contextmenu", this.showStatusPicker);
+            this.avatarElement.removeEventListener(this.settings.flip?"contextmenu":"click", this.showUserPopout);//originaly click
+            this.avatarElement.removeEventListener(this.settings.flip?"click":"contextmenu", this.showStatusPicker);//originaly contextmenu
         }
 
-        addAllListeners() {            
+        addAllListeners() {
             if (this.settings.nickname.showNickname || this.settings.nickname.oppositeOnHover) document.addEventListener("mousemove", this.adjustNickname);
 
             if (!this.tagElement) return Logger.err("Tag element not available");
@@ -133,19 +229,19 @@ module.exports = (() => {
             if (this.settings.popout.username) {
                 PluginUtilities.addStyle(this.getName() + "-css", usernameCSS);
                 this.tagElement.addEventListener("mousedown", this.updateIsPopoutOpen);
-                this.tagElement.addEventListener("click", this.showUserPopout);
+                this.tagElement.addEventListener(flipmouse[!flipper], this.showUserPopout);//originaly click
             }
             if (this.settings.popout.avatar) {
                 this.tagElement.addEventListener("mousedown", this.updateIsPopoutOpen);
-                this.avatarElement.addEventListener("click", this.showUserPopout);
+                this.avatarElement.addEventListener(this.settings.flip?"contextmenu":"click", this.showUserPopout);//originaly click
             }
             if (this.settings.statusPicker.username) {
                 this.tagElement.addEventListener("mousedown", this.updateIsPopoutOpen);
-                this.tagElement.addEventListener("contextmenu", this.showStatusPicker);
+                this.tagElement.addEventListener(this.settings.flip?"click":"contextmenu", this.showStatusPicker);//originaly contextmenu
             }
             if (this.settings.statusPicker.avatar) {
                 this.avatarElement.addEventListener("mousedown", this.updateIsPopoutOpen);
-                this.avatarElement.addEventListener("contextmenu", this.showStatusPicker);
+                this.avatarElement.addEventListener(this.settings.flip?"click":"contextmenu", this.showStatusPicker);//originaly contextmenu
             }
         }
 
