b74454623e4cec644d138002956f09895db08c4c
Leopixelo <kleinerbear2006@gmail.com>
2021-07-15 16:29:51 +0200

    Fixed missing PermissionViewer button for text channels

--- src/plugins/PermissionsViewer/index.js
+++ src/plugins/PermissionsViewer/index.js
@@ -153,7 +153,7 @@ module.exports = (Plugin, Api) => {
 
         patchChannelContextMenu() {
             const [VoiceChannelContextMenu] = WebpackModules.getModules(m => m.default && m.default.displayName == "ChannelListVoiceChannelContextMenu");
-            const [, CategoryChannelContextMenu, TextChannelContextMenu] = WebpackModules.getModules(m => m.default && m.default.displayName == "ChannelListTextChannelContextMenu");
+            const [CategoryChannelContextMenu, , TextChannelContextMenu] = WebpackModules.getModules(m => m.default && m.default.displayName == "ChannelListTextChannelContextMenu");
             const patch = (_, [props], retVal) => {
                 const original = retVal.props.children[0].props.children;
                 const newOne = DCM.buildMenuItem({
