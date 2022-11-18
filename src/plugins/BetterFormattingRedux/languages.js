7dc06324df5affdf6df571317e3fcc1c45fbd882
KIRISHIMA Touka <tkirishima@tkirishima.week.golf>
2022-11-18 20:29:35 +0100

    Added more basic programming languages in BetterFormattingRedux plugin

--- src/plugins/BetterFormattingRedux/languages.js
+++ src/plugins/BetterFormattingRedux/languages.js
@@ -1,10 +1,25 @@
 module.exports = {
-    C: {cpp: "C++", csharp: "C#", coffeescript: "CoffeeScript", css: "CSS"},
-    H: {html: "HTML/XML"},
-    J: {java: "Java", js: "JavaScript", json: "JSON"},
+    A: {ada: "Ada", awk: "Awk"},
+    B: {bash: "Bash"},
+    C: {c: "c", clj: "Clojure", coffeescript: "CoffeeScript", cpp: "C++", crystal: "Crystal", csharp: "C#", css: "CSS"},
+    D: {d: "D", dart: "Dart", dockerfile: "Dockerfile"},
+    E: {elixir: "Elixir", erl: "Erlang"},
+    F: {fs: "F#"},
+    H: {hs: "Haskell", html: "HTML/XML"},
+    J: {java: "Java", js: "JavaScript", json: "JSON", julia: "Julia"},
+    K: {kt: "Kotlin"},
+    L: {latex: "LaTeX", lisp: "Lisp", lua: "Lua"},
+    O: {ml: "OCaml"},
     M: {markdown: "Markdown"},
-    P: {perl: "Perl", php: "PHP", py: "Python"},
-    R: {ruby: "Ruby"},
-    S: {sql: "SQL"},
-    V: {vbnet: "VB.NET", vhdl: "VHDL"}
+    N: {nim: "Nim"},
+    P: {perl: "Perl", php: "PHP", powershell: "Powershell", prolog: "Prolog", py: "Python"},
+    R: {r: "R", rs: "Rust", ruby: "Ruby"},
+    S: {ts: "TypeScript"},
+    V: {vbnet: "VB.NET", vhdl: "VHDL"},
+    Z: {zsh: "ZSH"},
 };
+
+// Log the Object sorted
+// Object.keys(module.exports).forEach(function(key, index) {
+//     console.log(`${key}: {${Object.keys(module.exports[key]).sort().map(str => `${str}: "${module.exports[key][str]}"`).join(', ')}},`)
+// });
