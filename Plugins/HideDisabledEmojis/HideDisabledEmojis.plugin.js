f1add341ac95e7d302f8eed6f2d4dc072a338604
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-11-26 03:21:19 +0530

    fixed
    it was not working as you would have known so fixed it

--- Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js
+++ Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name HideDisabledEmojis
  * @description Hides disabled emojis from the emoji picker.
- * @version 0.0.8
+ * @version 0.0.9
  * @author Zerebos
  * @authorId 249746236008169473
  * @website https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/HideDisabledEmojis
@@ -41,7 +41,7 @@ const config = {
                 twitter_username: "ZackRauen"
             }
         ],
-        version: "0.0.8",
+        version: "0.0.9",
         description: "Hides disabled emojis from the emoji picker.",
         github: "https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/HideDisabledEmojis",
         github_raw: "https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js"
@@ -85,65 +85,33 @@ if (!global.ZeresPluginLibrary) {
 }
  
 module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
-     const plugin = (Plugin, Api) => {
-    const {Patcher, DiscordModules, WebpackModules, Utilities} = Api;
+    const {Patcher, WebpackModules, Utilities} = Api;
+    const PickerWrapMemo = WebpackModules.getModule(m => m?.type?.render?.toString().includes(".emojiSize"));
     return class HideDisabledEmojis extends Plugin {
-        async onStart() {            
-            Patcher.after(DiscordModules.EmojiInfo, "isEmojiFiltered", (thisObject, methodArguments, returnValue) => {
-                return returnValue || DiscordModules.EmojiInfo.isEmojiDisabled(methodArguments[0], methodArguments[1]);
-            });
-
-            const doFiltering = props => {
-                props.rowCountBySection = props.rowCountBySection.filter((c, i) => c || props.collapsedSections.has(props.sectionDescriptors[i].sectionId));
-                props.sectionDescriptors = props.sectionDescriptors.filter(s => s.count || props.collapsedSections.has(s.sectionId));
-                
-                const wasFiltered = props.emojiGrid.filtered;
-                props.emojiGrid = props.emojiGrid.filter(r => r.length);
-                if (wasFiltered) props.emojiGrid.filtered = true; // Reassign
-            };
-
-            const PickerWrapMemo = WebpackModules.getModule(m => m?.type?.render.toString().includes("emoji"));
-            Patcher.after(PickerWrapMemo.type, "render", (_, __, ret) => {
-                const pickerChild = Utilities.findInTree(ret, m => m?.props?.emojiGrid, {walkable: ["props", "children"]});
-                if (!pickerChild?.type?.type) return;
-                if (pickerChild.type.type.__patched) return;
+        async onStart() { 
+            Patcher.after(PickerWrapMemo.type, "render", (_, args, res) => {
+              const pickerChild = Utilities.findInTree(res, m => m?.props?.emojiGrid, {walkable: ["props", "children"]});
                 Patcher.before(pickerChild.type, "type", (_, [props]) => {
-                    if (!props.rowCountBySection) return;
-                    if (props.emojiGrid.filtered) return doFiltering(props);
-                    props.emojiGrid.filtered = true;
-                    let row = 0;
-                    for (let s = 0; s < props.sectionDescriptors.length; s++) {
-                        const section = props.sectionDescriptors[s];
-                        const rowCount = props.rowCountBySection[s];
-                        const rowEnd = row + rowCount - 1;
-                        let countLeft = 0;
-                        let rowsLeft = 0;
-                        for (let r = row; r <= rowEnd; r++) {
-                            props.emojiGrid[r] = props.emojiGrid[r].filter(e => !e.isDisabled);
-                            const remaining = props.emojiGrid[r].length;
-                            if (remaining) {
-                                rowsLeft = rowsLeft + 1;
-                                countLeft = countLeft + remaining;
-                            }
-                        }
-                        section.count = countLeft;
-                        props.rowCountBySection[s] = rowsLeft;
-    
-                        row = rowEnd + 1;
-                    }
-    
-                    doFiltering(props);
-                });
-                pickerChild.type.type.__patched = true;
+                  props.emojiGrid = props.emojiGrid.map(m => m.filter(m => !m.isDisabled)).filter(m => m.length);
+                  props.rowCount = props.emojiGrid.length;
+                  if (!props.sectionDescriptors) return;
+                  const mappedEmojiCount = {};
+                  props.emojiGrid.flat(10).forEach(item => {
+                    item?.category?.toLowerCase() == "recent" ||  item?.category?.toLowerCase() == "unicode" ? mappedEmojiCount[item.category] = (mappedEmojiCount[item.category] || 0) + 1 : mappedEmojiCount[item?.emoji?.guildId] = (mappedEmojiCount[item?.emoji?.guildId] || 0) + 1 ;
+                  });                  
+                  props.sectionDescriptors = props.sectionDescriptors.map(m => {
+                    if (m?.type?.toLowerCase() == "unicode") return m;
+                    m.count =  mappedEmojiCount[m.type] ? mappedEmojiCount[m.type] :  mappedEmojiCount[m.guild.id]
+                    return m;
+                  }).filter(m => m.count);
+                 props.rowCountBySection =  props.sectionDescriptors.map(m => Math.ceil(m.count / 9));                  
+              });               
             });
-        }
-        
+        }        
         onStop() {
             Patcher.unpatchAll();
         }
-
     };
-};
      return plugin(Plugin, Api);
 })(global.ZeresPluginLibrary.buildPlugin(config));
 /*@end@*/
